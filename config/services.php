3c1983e342ef7a7334555bde55b50012dedd1881
Harbrinder Singh <harbrinder.c@gmail.com>
2020-07-23 02:35:37 -0400

    Secret keys for Microservices

--- config/services.php
+++ config/services.php
@@ -3,9 +3,11 @@
 return [
     'authors' => [
         'base_uri' => env('AUTHORS_SERVICES_BASE_URL'),
+        'secret' => env('AUTHORS_SERVICES_SECRET'),
     ],
 
     'books' => [
         'base_uri' => env('BOOKS_SERVICES_BASE_URL'),
+        'secret' => env('AUTHORS_SERVICES_SECRET'),
     ]
 ];
